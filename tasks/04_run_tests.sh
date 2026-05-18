55b64cf799fc2f8aca9a7fa5de7dfd510de9a643
Márton Spiteller <spitellermarton@gmail.com>
2026-05-18 09:55:41 +0200

    update: test runner NODE_OPTIONS flag and README.md

--- tasks/04_run_tests.sh
+++ tasks/04_run_tests.sh
@@ -7,9 +7,9 @@ npm install --silent --save-dev jest-silent-reporter
 
 # NODE_OPTIONS flags needed for node.js 25+, otherwise it writes warnings to stdout 
 # --no-experimental-webstorage or --no-warnings
-NODE_OPTIONS="--no-warnings" npx jest --silent \
+NODE_OPTIONS="--no-experimental-webstorage" npx jest --silent \
   --coverage \
-  --coverageDirectory="$WORKDIR/artefacts/test-coverage" \
+  --coverageDirectory="$WORKDIR/artifacts/test-coverage" \
   --coverageReporters json \
   --coverageReporters html \
   --coverageReporters lcov \
