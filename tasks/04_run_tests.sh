4b486d023abad3141b6462c13da3a199482850c1
Márton Spiteller <spitellermarton@gmail.com>
2026-05-18 09:35:00 +0200

    update: test runner to suppress writing to the console

--- tasks/04_run_tests.sh
+++ tasks/04_run_tests.sh
@@ -4,15 +4,14 @@ source "$PWD/config.sh"
 
 cd "$WORKDIR/repo"
 npm install --silent --save-dev jest-silent-reporter
-#npx jest --silent --coverage --coverageReporters="json,html,lcov" --reporters=jest-silent-reporter
 
-npx jest --silent \
+# NODE_OPTIONS flags needed for node.js 25+, otherwise it writes warnings to stdout 
+# --no-experimental-webstorage or --no-warnings
+NODE_OPTIONS="--no-warnings" npx jest --silent \
   --coverage \
-  --coverageReporters="json" \
-  --coverageReporters="html" \
-  --coverageReporters="lcov" \
-  --reporters=jest-silent-reporter \
+  --coverageDirectory="$WORKDIR/artefacts/test-coverage" \
+  --coverageReporters json \
+  --coverageReporters html \
+  --coverageReporters lcov \
+  --reporters jest-silent-reporter \
   --noStackTrace
-
-#npx jest --silent # --coverage --coverageDirectory="$WORKDIR/artefacts/test-coverage" --silent
-#mv "$WORKDIR/repo/coverage" "$WORKDIR/artefacts"
